66883a30fae588ffb1a58923d67c59d27381bc39
David Ball <david@daball.me>
2024-07-18 02:05:05 -0400

    Refactored model/GetVariationsRequest and model/GetVariationsResponse.

--- src/model/GetVariationsRequest.ts
+++ src/model/GetVariationsRequest.ts
@@ -21,57 +21,88 @@
  *
  */
 
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/Condition', 'model/GetVariationsResource', 'model/Merchant', 'model/OfferCount', 'model/PartnerType', 'model/Properties'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('./Condition'), require('./GetVariationsResource'), require('./Merchant'), require('./OfferCount'), require('./PartnerType'), require('./Properties'));
-  } else {
-    // Browser globals (root is window)
-    if (!root.ProductAdvertisingAPIv1) {
-      root.ProductAdvertisingAPIv1 = {};
-    }
-    root.ProductAdvertisingAPIv1.GetVariationsRequest = factory(root.ProductAdvertisingAPIv1.ApiClient, root.ProductAdvertisingAPIv1.Condition, root.ProductAdvertisingAPIv1.GetVariationsResource, root.ProductAdvertisingAPIv1.Merchant, root.ProductAdvertisingAPIv1.OfferCount, root.ProductAdvertisingAPIv1.PartnerType, root.ProductAdvertisingAPIv1.Properties);
-  }
-}(this, function(ApiClient, Condition, GetVariationsResource, Merchant, OfferCount, PartnerType, Properties) {
-  'use strict';
+import { ApiClient } from "../ApiClient";
+import { Condition } from "./Condition";
+import { GetVariationsResource } from "./GetVariationsResource";
+import { Merchant } from "./Merchant";
+import { OfferCount } from "./OfferCount";
+import { PartnerType } from "./PartnerType";
+import { Properties } from "./Properties";
 
+/**
+ * The GetVariationsRequest model module.
+ * @module model/GetVariationsRequest
+ * @version 1.0.0
+ */
 
-
-
+/**
+ * Constructs a new <code>GetVariationsRequest</code>.
+ * @alias module:model/GetVariationsRequest
+ * @class
+ * @param ASIN {String} 
+ * @param partnerTag {String} 
+ * @param partnerType {module:model/PartnerType} 
+ */
+export class GetVariationsRequest {
   /**
-   * The GetVariationsRequest model module.
-   * @module model/GetVariationsRequest
-   * @version 1.0.0
+   * @member {String} ASIN
    */
-
+  public ASIN?: string;
   /**
-   * Constructs a new <code>GetVariationsRequest</code>.
-   * @alias module:model/GetVariationsRequest
-   * @class
-   * @param ASIN {String} 
-   * @param partnerTag {String} 
-   * @param partnerType {module:model/PartnerType} 
+   * @member {module:model/Condition} Condition
    */
-  var exports = function(ASIN, partnerTag, partnerType) {
-    var _this = this;
+  public Condition?: Condition;
+  /**
+   * @member {String} CurrencyOfPreference
+   */
+  public CurrencyOfPreference?: string;
+  /**
+   * @member {Array.<String>} LanguagesOfPreference
+   */
+  public LanguagesOfPreference?: string[];
+  /**
+   * @member {String} Marketplace
+   */
+  public Marketplace?: string;
+  /**
+   * @member {module:model/Merchant} Merchant
+   */
+  public Merchant?: Merchant;
+  /**
+   * @member {module:model/OfferCount} OfferCount
+   */
+  public OfferCount?: OfferCount
+  /**
+   * @member {String} PartnerTag
+   */
+  public PartnerTag?: string;
+  /**
+   * @member {module:model/PartnerType} PartnerType
+   */
+  public PartnerType?: PartnerType;
+  /**
+   * @member {module:model/Properties} Properties
+   */
+  public Properties?: Properties;
+  /**
+   * @member {Array.<module:model/GetVariationsResource>} Resources
+   */
+  public Resources?: GetVariationsResource[];
+  /**
+   * @member {Number} VariationCount
+   */
+  public VariationCount?: number;
+  /**
+   * @member {Number} VariationPage
+   */
+  public VariationPage?: number;
 
-    _this['ASIN'] = ASIN;
-
-
-
-
-
-
-    _this['PartnerTag'] = partnerTag;
-    _this['PartnerType'] = partnerType;
-
-
-
-
-  };
+  // not sure whether I need this or not:
+  // public constructor(ASIN, partnerTag, partnerType) {
+  //   this.ASIN = ASIN;
+  //   this.PartnerTag = partnerTag;
+  //   this.PartnerType = partnerType;
+  // };
 
   /**
    * Constructs a <code>GetVariationsRequest</code> from a plain JavaScript object, optionally creating a new instance.
@@ -80,109 +111,50 @@
    * @param {module:model/GetVariationsRequest} obj Optional instance to populate.
    * @return {module:model/GetVariationsRequest} The populated <code>GetVariationsRequest</code> instance.
    */
-  exports.constructFromObject = function(data, obj) {
+  public static constructFromObject(data: any, obj?: GetVariationsRequest) {
     if (data) {
-      obj = obj || new exports();
+      obj = obj || new GetVariationsRequest();
 
       if (data.hasOwnProperty('ASIN')) {
-        obj['ASIN'] = ApiClient.convertToType(data['ASIN'], 'String');
+        obj.ASIN = ApiClient.convertToType(data['ASIN'], 'String');
       }
       if (data.hasOwnProperty('Condition')) {
-        obj['Condition'] = Condition.constructFromObject(data['Condition']);
+        obj.Condition = Condition.constructFromObject(data['Condition']);
       }
       if (data.hasOwnProperty('CurrencyOfPreference')) {
-        obj['CurrencyOfPreference'] = ApiClient.convertToType(data['CurrencyOfPreference'], 'String');
+        obj.CurrencyOfPreference = ApiClient.convertToType(data['CurrencyOfPreference'], 'String');
       }
       if (data.hasOwnProperty('LanguagesOfPreference')) {
-        obj['LanguagesOfPreference'] = ApiClient.convertToType(data['LanguagesOfPreference'], ['String']);
+        obj.LanguagesOfPreference = ApiClient.convertToType(data['LanguagesOfPreference'], ['String']);
       }
       if (data.hasOwnProperty('Marketplace')) {
-        obj['Marketplace'] = ApiClient.convertToType(data['Marketplace'], 'String');
+        obj.Marketplace = ApiClient.convertToType(data['Marketplace'], 'String');
       }
       if (data.hasOwnProperty('Merchant')) {
-        obj['Merchant'] = Merchant.constructFromObject(data['Merchant']);
+        obj.Merchant = Merchant.constructFromObject(data['Merchant']);
       }
       if (data.hasOwnProperty('OfferCount')) {
-        obj['OfferCount'] = OfferCount.constructFromObject(data['OfferCount']);
+        obj.OfferCount = OfferCount.constructFromObject(data['OfferCount']);
       }
       if (data.hasOwnProperty('PartnerTag')) {
-        obj['PartnerTag'] = ApiClient.convertToType(data['PartnerTag'], 'String');
+        obj.PartnerTag = ApiClient.convertToType(data['PartnerTag'], 'String');
       }
       if (data.hasOwnProperty('PartnerType')) {
-        obj['PartnerType'] = PartnerType.constructFromObject(data['PartnerType']);
+        obj.PartnerType = PartnerType.constructFromObject(data['PartnerType']);
       }
       if (data.hasOwnProperty('Properties')) {
-        obj['Properties'] = Properties.constructFromObject(data['Properties']);
+        obj.Properties = Properties.constructFromObject(data['Properties']);
       }
       if (data.hasOwnProperty('Resources')) {
-        obj['Resources'] = ApiClient.convertToType(data['Resources'], [GetVariationsResource]);
+        obj.Resources = ApiClient.convertToType(data['Resources'], [GetVariationsResource]);
       }
       if (data.hasOwnProperty('VariationCount')) {
-        obj['VariationCount'] = ApiClient.convertToType(data['VariationCount'], 'Number');
+        obj.VariationCount = ApiClient.convertToType(data['VariationCount'], 'Number');
       }
       if (data.hasOwnProperty('VariationPage')) {
-        obj['VariationPage'] = ApiClient.convertToType(data['VariationPage'], 'Number');
+        obj.VariationPage = ApiClient.convertToType(data['VariationPage'], 'Number');
       }
     }
     return obj;
   }
-
-  /**
-   * @member {String} ASIN
-   */
-  exports.prototype['ASIN'] = undefined;
-  /**
-   * @member {module:model/Condition} Condition
-   */
-  exports.prototype['Condition'] = undefined;
-  /**
-   * @member {String} CurrencyOfPreference
-   */
-  exports.prototype['CurrencyOfPreference'] = undefined;
-  /**
-   * @member {Array.<String>} LanguagesOfPreference
-   */
-  exports.prototype['LanguagesOfPreference'] = undefined;
-  /**
-   * @member {String} Marketplace
-   */
-  exports.prototype['Marketplace'] = undefined;
-  /**
-   * @member {module:model/Merchant} Merchant
-   */
-  exports.prototype['Merchant'] = undefined;
-  /**
-   * @member {module:model/OfferCount} OfferCount
-   */
-  exports.prototype['OfferCount'] = undefined;
-  /**
-   * @member {String} PartnerTag
-   */
-  exports.prototype['PartnerTag'] = undefined;
-  /**
-   * @member {module:model/PartnerType} PartnerType
-   */
-  exports.prototype['PartnerType'] = undefined;
-  /**
-   * @member {module:model/Properties} Properties
-   */
-  exports.prototype['Properties'] = undefined;
-  /**
-   * @member {Array.<module:model/GetVariationsResource>} Resources
-   */
-  exports.prototype['Resources'] = undefined;
-  /**
-   * @member {Number} VariationCount
-   */
-  exports.prototype['VariationCount'] = undefined;
-  /**
-   * @member {Number} VariationPage
-   */
-  exports.prototype['VariationPage'] = undefined;
-
-
-
-  return exports;
-}));
-
-
+};
--- src/model/GetVariationsResponse.ts
+++ src/model/GetVariationsResponse.ts
@@ -15,49 +15,36 @@
  * permissions and limitations under the License.
  */
 
- /**
+/**
  * ProductAdvertisingAPI
  * https://webservices.amazon.com/paapi5/documentation/index.html
  *
  */
 
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/ErrorData', 'model/VariationsResult'], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('./ErrorData'), require('./VariationsResult'));
-  } else {
-    // Browser globals (root is window)
-    if (!root.ProductAdvertisingAPIv1) {
-      root.ProductAdvertisingAPIv1 = {};
-    }
-    root.ProductAdvertisingAPIv1.GetVariationsResponse = factory(root.ProductAdvertisingAPIv1.ApiClient, root.ProductAdvertisingAPIv1.ErrorData, root.ProductAdvertisingAPIv1.VariationsResult);
-  }
-}(this, function(ApiClient, ErrorData, VariationsResult) {
-  'use strict';
+/**
+ * The GetVariationsResponse model module.
+ * @module model/GetVariationsResponse
+ * @version 1.0.0
+ */
 
+import { ApiClient } from "../ApiClient";
+import { ErrorData } from "./ErrorData";
+import { VariationsResult } from "./VariationsResult";
 
-
-
+/**
+ * Constructs a new <code>GetVariationsResponse</code>.
+ * @alias module:model/GetVariationsResponse
+ * @class
+ */
+export class GetVariationsResponse {
   /**
-   * The GetVariationsResponse model module.
-   * @module model/GetVariationsResponse
-   * @version 1.0.0
+   * @member {Array.<module:model/ErrorData>} Errors
    */
-
+  public Errors?: ErrorData[]
   /**
-   * Constructs a new <code>GetVariationsResponse</code>.
-   * @alias module:model/GetVariationsResponse
-   * @class
+   * @member {module:model/VariationsResult} VariationsResult
    */
-  var exports = function() {
-    var _this = this;
-
-
-
-  };
+  public VariationsResult?: VariationsResult;
 
   /**
    * Constructs a <code>GetVariationsResponse</code> from a plain JavaScript object, optionally creating a new instance.
@@ -66,32 +53,17 @@
    * @param {module:model/GetVariationsResponse} obj Optional instance to populate.
    * @return {module:model/GetVariationsResponse} The populated <code>GetVariationsResponse</code> instance.
    */
-  exports.constructFromObject = function(data, obj) {
+  public static constructFromObject(data: any, obj?: GetVariationsResponse) {
     if (data) {
-      obj = obj || new exports();
+      obj = obj || new GetVariationsResponse();
 
       if (data.hasOwnProperty('Errors')) {
-        obj['Errors'] = ApiClient.convertToType(data['Errors'], [ErrorData]);
+        obj.Errors = ApiClient.convertToType(data['Errors'], [ErrorData]);
       }
       if (data.hasOwnProperty('VariationsResult')) {
-        obj['VariationsResult'] = VariationsResult.constructFromObject(data['VariationsResult']);
+        obj.VariationsResult = VariationsResult.constructFromObject(data['VariationsResult']);
       }
     }
     return obj;
   }
-
-  /**
-   * @member {Array.<module:model/ErrorData>} Errors
-   */
-  exports.prototype['Errors'] = undefined;
-  /**
-   * @member {module:model/VariationsResult} VariationsResult
-   */
-  exports.prototype['VariationsResult'] = undefined;
-
-
-
-  return exports;
-}));
-
-
+};
